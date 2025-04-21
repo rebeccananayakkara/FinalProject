be233bcf306fa893e04c10c15e209700972f2c78
rnana <rebeccananayakkara04@gmail.com>
2025-04-21 14:37:04 -0400

    added the 30 courses plus the five classrooms

--- test.py
+++ test.py
@@ -1,1 +1,43 @@
-print('Hello World')
+#By Lily and Rebecca for CMPSC 463
+#Project 2: Class Scheduling
+#Look into 30 CMPSC/IST courses, schedule them into 5 classrooms from 7am to 6pm
+# Greedy algorithm
+
+# differerent courses:
+# 1) CMPSC 312 - Computer Organization and Architecture
+# 2) CMPSC 430 - Database Design
+# 3) CMPSC 462 - Data Structures
+# 4) CMPSC 463 - Design and Analysis of Algorithms
+# 5) CMPSC 469 - Formal Languages with Applications
+# 6) CMPSC 472 - Operation System Concepts
+# 7) CMPSC 487W - Software Engineering and Design
+# 8) CMPSC 330 - Advanced Programming in C++
+# 9) CMPSC 360 - Discrete Mathematics for Computer Science
+# 10) CMPSC 441 - Artificial Intelligence
+# 11) CMPSC 445 - Applied Machine Learning in Data Science
+# 12) CMPSC 446 - Data Mining
+# 13) CMPSC 457 - Computer Graphics Algorithms
+# 14) CMPSC 497 - Special Topics
+# 15) CMSPC 313 - Assembly Language Programming
+# 16) CMPSC 470 - Compiler Construction
+# 17) CMSPC 221 - Object Oriented Programming with Web-Based Applications
+# 18) CMPSC 412 - Data Structures Lab
+# 19) CMPSC 413 - Algorithms Lab
+# 20) CMPSC 475 - Applications Programming
+# 21) CMPSC 496 - Independent Studies
+# 22) CMPSC 414 - Contest Programming
+# 23) CMPSC 421 - Net-centric Computing
+# 24) CMPSC 444 - Secure Programming
+# 25) CMPSC 438 - Computer Network Architecture and Programming
+# 26) CMPSC 489 - Deep Learning for Computer Vision
+# 27) CMPSC 488 - Computer Science Project
+# 28) CMPSC 201 - Programming for Engineers with C++
+# 29) CMPSC 131 - Programming and Computation I: Fundamentals
+# 30) CMPSC 132 - Programming and Computation II: Data Structures
+
+#Classes availiable from 7am - 6pm:
+# 1) Woodland Building 342
+# 2) Woodland Building 345
+# 3) Woodland Building 343
+# 4) Woodland Building 346
+# 5) Sutherland Building 209
